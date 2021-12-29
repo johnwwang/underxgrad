4dad0cf9c2eaece2f394c47a7dc98a336ef25c61
John Wang <johnwang58@gmail.com>
2021-12-29 17:41:00 -0600

    Skeleton with comments for scoring and matching algorithms

---
+++ matching.py
@@ -0,0 +1,22 @@
+import numpy as np
+from typing import List, Tuple
+
+def matching(scores: List[List], gender_id: List, gender_pref: List) -> List[Tuple]:
+    matches = [()]
+    return matches
+
+if __name__ == "__main__":
+    raw_scores = np.loadtxt('raw_scores.txt').tolist()
+    genders = []
+    with open('genders.txt', 'r') as file:
+        for line in file:
+            curr = line[:-1]
+            genders.append(curr)
+
+    gender_preferences = []
+    with open('gender_preferences.txt', 'r') as file:
+        for line in file:
+            curr = line[:-1]
+            gender_preferences.append(curr)
+
+    gs_matches = run_matching(raw_scores, genders, gender_preferences)
---
+++ scoring.py
@@ -0,0 +1,14 @@
+import numpy as np
+from typing import List
+
+def scoring() -> List[int]:
+    #score based on certain metrics
+
+    #1. metric based on interests - 
+    #2. metric based on career paths - drop down menu
+    #3. metric based on personal traits - some personal questions and a written personal statement
+    return
+
+if __name__ == "__main__":
+    scores = scoring()
+    print(scores)
