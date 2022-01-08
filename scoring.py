289cd903a4868c7c33541c297db833a42052c924
John Wang <johnwang58@gmail.com>
2022-01-08 11:10:49 -0600

    This commit adds the infrastructure for Flaskjs, Reactjs, MongoDB, and Docker.
    Things to set up:
    
    1. MongoDB database and user system
    2. Simple production server

--- scoring.py
+++ scoring.py
@@ -7,6 +7,12 @@ def scoring() -> List[int]:
     #1. metric based on interests - 
     #2. metric based on career paths - drop down menu
     #3. metric based on personal traits - some personal questions and a written personal statement
+
+    #prediction market ideas.
+
+    # 1. have algorithm for matching 
+    # 2.
+
     return
 
 if __name__ == "__main__":
